798efa3208ac1b8315f7470f7fa5d7c8aacec8e1
little-bear-king <ryanburkle95@gmail.com>
2023-04-25 01:18:19 -0500

    Changed the colorscheme to Melange for the nice warm colors

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,11 +1,9 @@
 local M = {
-  "folke/tokyonight.nvim",
-  commit = "e52c41314e83232840d6970e6b072f9fba242eb9",
-  lazy = false,    -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
+    "savq/melange-nvim",
+    lazy = false,
+    priority = 1000,
 }
-
-M.name = "tokyonight-night"
+M.name = 'melange'
 function M.config()
   local status_ok, _ = pcall(vim.cmd.colorscheme, M.name)
   if not status_ok then
