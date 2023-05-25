9d38a6ccb305a629c5bab9b91eb9fb8c8ef7fa11
Little Bear King <ryanburkle95@gmail.com>
2023-05-25 03:37:44 -0500

    IT WORKS. I got rid of the commits on the treesitter stuff

--- lua/user/comment.lua
+++ lua/user/comment.lua
@@ -1,12 +1,12 @@
 local M = {
   "numToStr/Comment.nvim",
-  commit = "eab2c83a0207369900e92783f56990808082eac2",
+  -- commit = "eab2c83a0207369900e92783f56990808082eac2",
   event = "BufRead",
   dependencies = {
     {
       "JoosepAlviste/nvim-ts-context-commentstring",
       event = "VeryLazy",
-      commit = "a0f89563ba36b3bacd62cf967b46beb4c2c29e52",
+      -- commit = "a0f89563ba36b3bacd62cf967b46beb4c2c29e52",
     },
   },
 }
--- lua/user/illuminate.lua
+++ lua/user/illuminate.lua
@@ -1,7 +1,7 @@
 local M = {
   "RRethy/vim-illuminate",
   commit = "d6ca7f77eeaf61b3e6ce9f0e5a978d606df44298",
-  event = "VeryLazy",
+  -- event = "VeryLazy",
 }
 
 function M.config()
